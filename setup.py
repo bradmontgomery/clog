424dcc669a15096a5e092d4a15fd642ba670aa11
Brad Montgomery <brad@bradmontgomery.net>
2022-02-24 15:44:05 -0600

    version bump

--- setup.py
+++ setup.py
@@ -5,7 +5,7 @@ with open("README.md", "r", encoding="utf-8") as fh:
 
 setuptools.setup(
     name="clog",
-    version="0.3.0",
+    version="0.3.1",
     author="Brad Montgomery",
     author_email="brad@bradmontgomery.net",
     description="pretty-print with color",
